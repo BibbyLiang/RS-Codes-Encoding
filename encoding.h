051c5193dfc235bc7adb527bcf95ab48cd1e0629
Liang Jiwei <ljw_seu@163.com>
2021-09-13 20:57:13 +0800

    Add files via upload

--- encoding.h
+++ encoding.h
@@ -1,8 +1,15 @@
 #ifndef ENCODING_H
 #define ENCODING_H
 
+#define GF_16_CASE		 0
+
+#if (1 == GF_16_CASE)
 #define GF_FIELD        16
 #define MESSAGE_LEN     9
+#else
+#define GF_FIELD        8
+#define MESSAGE_LEN     3
+#endif
 #define CODEWORD_LEN    GF_FIELD - 1    
 
 extern unsigned char power_polynomial_table[GF_FIELD][2];
